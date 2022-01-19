edf88735dd0788bfcab880a61a40b65dfd98b7f0
Radu Necsanu <radunecsanu@Radus-MacBook-Pro.local>
2022-01-19 10:52:03 +0000

    RN: ready: cleaned up for recording

--- client/src/components/main-container.js
+++ client/src/components/main-container.js
@@ -1,7 +1,5 @@
 import React, { Component, useState } from "react";
 import SomeComponentForTestPilotNoTest from "./some-component-no-test";
-import SomeComponentForTestPilot from "./some-component";
-//
 
 const MainComponent = (props) => {
   const [count, setCount] = useState(0);
@@ -9,8 +7,6 @@ const MainComponent = (props) => {
     <div onClick={() => setCount(count + 1)}>
       <h1>testPilot {count}</h1>
       <SomeComponentForTestPilotNoTest color="red" size={count} />
-      <br />
-      <SomeComponentForTestPilot color="red" size="33" />
     </div>
   );
 };
